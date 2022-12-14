c99113dab9474d6242cea77b1679e036ca36d83d
seanld03 <richriots@gmail.com>
2022-12-14 19:29:20 +0000

    small update to fix gui fix

--- src/main/java/com/kuri0/rawinput/RawInput.java
+++ src/main/java/com/kuri0/rawinput/RawInput.java
@@ -1,6 +1,7 @@
 package com.kuri0.rawinput;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
@@ -22,38 +23,44 @@ public class RawInput
     public static int dx = 0;
     public static int dy = 0;
 
+	public static void getMouse() {
+		for (int i = 0; i < controllers.length && mouse == null; i++) {
+			if (controllers[i].getType() == Controller.Type.MOUSE) {
+				controllers[i].poll();
+				if (((Mouse) controllers[i]).getX().getPollData() != 0.0 || ((Mouse) controllers[i]).getY().getPollData() != 0.0) {
+					mouse = (Mouse) controllers[i];
+				}
+			}
+		}
+	}
+
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-    	ClientCommandHandler.instance.registerCommand(new RescanCommand());
-	ClientCommandHandler.instance.registerCommand(new ToggleCommand());
-        Minecraft.getMinecraft().mouseHelper = new RawMouseHelper();
-	controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+		ClientCommandHandler.instance.registerCommand(new RescanCommand());
+		ClientCommandHandler.instance.registerCommand(new ToggleCommand());
+		Minecraft.getMinecraft().mouseHelper = new RawMouseHelper();
+		controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
 
         Thread inputThread = new Thread(() -> {
 			while(true){
-				for(int i = 0; i < controllers.length && mouse == null; i++){
-					if (controllers[i].getType() == Controller.Type.MOUSE) {
-						controllers[i].poll();
-						if (((Mouse)controllers[i]).getX().getPollData() != 0.0 || ((Mouse)controllers[i]).getY().getPollData() != 0.0) {
-							mouse = (Mouse)controllers[i];
-						}
-					}
-				}
-				if(mouse != null){
+				if (mouse != null && Minecraft.getMinecraft().currentScreen == null){
 					mouse.poll();
-
 					dx += (int)mouse.getX().getPollData();
 					dy += (int)mouse.getY().getPollData();
+				} else {
+					getMouse();
 				}
-				try{
+
+				try {
 					Thread.sleep(1);
-				}catch(InterruptedException e){
+				} catch(InterruptedException e) {
 					e.printStackTrace();
 				}
 			}
         });
-        inputThread.setName("inputThread");
+
+		inputThread.setName("inputThread");
         inputThread.start();
     }
 }
