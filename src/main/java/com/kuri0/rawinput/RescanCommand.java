dc37ae521808e2df9e38480fcabf2c608a73291d
seanld03 <richriots@gmail.com>
2022-12-14 22:13:21 +0000

    Update RescanCommand.java

--- src/main/java/com/kuri0/rawinput/RescanCommand.java
+++ src/main/java/com/kuri0/rawinput/RescanCommand.java
@@ -19,7 +19,10 @@ public class RescanCommand extends CommandBase {
 	@Override
 	public void execute(MinecraftServer server, ICommandSender sender, String[] args) throws CommandException {
 		sender.sendMessage(new TextComponentString("Rescanning input devices..."));
-		com.kuri0.rawinput.RawInput.mouse = null;
+		com.kuri0.rawinput.RawInput.getMouse();
+		if (RawInput.mouse != null) {
+			sender.sendMessage(new TextComponentString("Mouse Found."));
+		}
 	}
 	@Override
 	public int getRequiredPermissionLevel() {
