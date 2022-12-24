fdf4803f7e5071e67e851f4874e718aa7431483a
seanld03 <richriots@gmail.com>
2022-12-24 06:18:13 +0000

    Update RawMouseHelper.java
    Allows player rotation to be changed correctly

--- src/main/java/mod/seanld/rawinput/RawMouseHelper.java
+++ src/main/java/mod/seanld/rawinput/RawMouseHelper.java
@@ -1,14 +1,24 @@
 package mod.seanld.rawinput;
 import net.minecraft.util.MouseHelper;
+import org.lwjgl.input.Mouse;
 
 public class RawMouseHelper extends MouseHelper {
-	   
     @Override
     public void mouseXYChange()
     {
-        this.deltaX = RawInput.dx;
-        RawInput.dx = 0;
-        this.deltaY = -RawInput.dy;
-        RawInput.dy = 0;
+        this.deltaX = RawInputHandler.dx;
+        RawInputHandler.dx = 0;
+        this.deltaY = -RawInputHandler.dy;
+        RawInputHandler.dy = 0;
+    }
+    @Override
+    public void grabMouseCursor()
+    {
+        if (Boolean.parseBoolean(System.getProperty("fml.noGrab","false"))) return;
+        Mouse.setGrabbed(true);
+        this.deltaX = 0;
+        RawInputHandler.dx = 0;
+        this.deltaY = 0;
+        RawInputHandler.dy = 0;
     }
 }
