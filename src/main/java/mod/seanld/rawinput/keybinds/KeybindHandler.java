0db387d2da740dc2949e1cfdbe97e11ab8573012
seanld03 <richriots@gmail.com>
2022-12-24 06:15:20 +0000

    Create KeybindHandler.java
    Keybinds for toggling and rescanning

---
+++ src/main/java/mod/seanld/rawinput/keybinds/KeybindHandler.java
@@ -0,0 +1,33 @@
+package mod.seanld.rawinput.keybinds;
+
+import mod.seanld.rawinput.RawInputHandler;
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.common.gameevent.InputEvent;
+import org.lwjgl.input.Keyboard;
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraftforge.fml.client.registry.ClientRegistry;
+
+public class KeybindHandler {
+
+    public static KeyBinding toggleKey;
+    public static KeyBinding rescanKey;
+
+    public static void init() {
+        toggleKey = new KeyBinding("Toggle Raw Input", Keyboard.CHAR_NONE, "Raw Input");
+        ClientRegistry.registerKeyBinding(toggleKey);
+
+        rescanKey = new KeyBinding("Rescan Key", Keyboard.CHAR_NONE, "Raw Input");
+        ClientRegistry.registerKeyBinding(rescanKey);
+    }
+
+    @SubscribeEvent
+    public void onKeyInput(InputEvent.KeyInputEvent event) {
+        if (toggleKey.isPressed()) {
+            RawInputHandler.toggleRawInput();
+        }
+
+        if (rescanKey.isPressed()) {
+            RawInputHandler.rescan();
+        }
+    }
+}
