5b69207ba81f22a7f5a9d9815c1e54c18270f595
seanld03 <richriots@gmail.com>
2022-12-24 06:13:49 +0000

    Tidy main file

--- src/main/java/mod/seanld/rawinput/RawInput.java
+++ src/main/java/mod/seanld/rawinput/RawInput.java
@@ -1,66 +1,39 @@
 package mod.seanld.rawinput;
+
+import mod.seanld.rawinput.commands.RescanCommand;
+import mod.seanld.rawinput.commands.ToggleCommand;
+import mod.seanld.rawinput.keybinds.KeybindHandler;
 import net.minecraft.client.Minecraft;
 import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
-
-
-import net.java.games.input.Controller;
-import net.java.games.input.ControllerEnvironment;
-import net.java.games.input.Mouse;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod(modid = RawInput.MODID, version = RawInput.VERSION, name = "Raw Input", useMetadata = true)
 public class RawInput
 {
     public static final String MODID = "rawinput";
-    public static final String VERSION = "1.3.0";
-    public static Mouse mouse;
-    public static Controller[] controllers;
-    // Delta for mouse
-    public static int dx = 0;
-    public static int dy = 0;
+    public static final String VERSION = "1.4.0";
 
-	public static void getMouse() {
-		for (int i = 0; i < controllers.length && mouse == null; i++) {
-			if (controllers[i].getType() == Controller.Type.MOUSE) {
-				controllers[i].poll();
-				if (((Mouse) controllers[i]).getX().getPollData() != 0.0 || ((Mouse) controllers[i]).getY().getPollData() != 0.0) {
-					mouse = (Mouse) controllers[i];
-				}
-			}
-		}
-	}
-
-    @EventHandler
+	@SideOnly(Side.CLIENT)
+	@EventHandler
     public void init(FMLInitializationEvent event)
     {
 		ClientCommandHandler.instance.registerCommand(new RescanCommand());
 		ClientCommandHandler.instance.registerCommand(new ToggleCommand());
 		Minecraft.getMinecraft().mouseHelper = new RawMouseHelper();
-		controllers = ControllerEnvironment.getDefaultEnvironment().getControllers();
+		MinecraftForge.EVENT_BUS.register(new KeybindHandler());
 
-        Thread inputThread = new Thread(() -> {
-			while(true){
-				if (mouse != null && Minecraft.getMinecraft().currentScreen == null) {
-					mouse.poll();
-					dx += (int)mouse.getX().getPollData();
-					dy += (int)mouse.getY().getPollData();
-				} else if (mouse != null) {
-					mouse.poll();
-				} else {
-					getMouse();
-				}
-
-				try {
-					Thread.sleep(1);
-				} catch(InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-        });
-
-		inputThread.setName("inputThread");
-        inputThread.start();
+		RawInputHandler.init();
     }
+
+	@SideOnly(Side.CLIENT)
+	@EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		KeybindHandler.init();
+	}
 }
