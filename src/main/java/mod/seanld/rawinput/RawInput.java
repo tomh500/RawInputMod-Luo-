9eba4268a079f04cea08ae783f1f03cc6d72114d
seanld03 <richriots@gmail.com>
2023-05-08 14:45:09 +0100

    Bump

--- src/main/java/mod/seanld/rawinput/RawInput.java
+++ src/main/java/mod/seanld/rawinput/RawInput.java
@@ -17,12 +17,11 @@ import net.minecraftforge.fml.relauncher.SideOnly;
 public class RawInput
 {
     public static final String MODID = "rawinput";
-    public static final String VERSION = "1.4.0";
+    public static final String VERSION = "1.4.1";
 
 	@SideOnly(Side.CLIENT)
 	@EventHandler
-    public void init(FMLInitializationEvent event)
-    {
+    public void init(FMLInitializationEvent event) {
 		ClientCommandHandler.instance.registerCommand(new RescanCommand());
 		ClientCommandHandler.instance.registerCommand(new ToggleCommand());
 		Minecraft.getMinecraft().mouseHelper = new RawMouseHelper();
